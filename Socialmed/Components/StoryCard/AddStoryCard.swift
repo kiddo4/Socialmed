866fdb27204bd963735675f36836d31e821cff77
kiddo4 <olanrewajutaiwo183@gmail.com>
2024-12-28 07:07:50 -0800

    done with the app

---
+++ Socialmed/Components/StoryCard/AddStoryCard.swift
@@ -0,0 +1,18 @@
+//
+//  AddStoryCard.swift
+//  Socialmed
+//
+//  Created by kiddo   on 28/12/2024.
+//
+
+import SwiftUI
+
+struct AddStoryCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddStoryCard()
+}
---
+++ Socialmed/Components/StoryCard/StoryCard.swift
@@ -0,0 +1,18 @@
+//
+//  StoryCard.swift
+//  Socialmed
+//
+//  Created by kiddo   on 28/12/2024.
+//
+
+import SwiftUI
+
+struct StoryCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryCard()
+}
